2acb42ed3d2183f7bebf933715278a8b48cfd84c
pkt117 <74464495+pkt117@users.noreply.github.com>
2022-01-19 22:49:03 +0900

    CSS : Responsive & Modify

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-document.querySelectorAll(".category__btn").forEach((element) =>
-  element.addEventListener("click", () => {
-    element.classList.toggle("active");
-  })
+// work에서의 button 이벤트
+
+const btn_click = document.querySelectorAll(".category__btn");
+
+btn_click.forEach(
+  (element) => element.addEventListener("click", (event) => handleClick(event))
+  // (event) => handleClick(event)를 handleClick로 줄여서가능
 );
-// 클릭시 다른 버튼은 불 active가 remove 되게 할것...
+
+function handleClick(event) {
+  if (event.target.classList[1] === "active") {
+    event.target.classList.remove("active");
+  } else {
+    btn_click.forEach((element) => element.classList.remove("active"));
+    event.target.classList.add("active");
+  }
+}
+
+// navbar 메뉴바 이벤트
+
+const nav_menu = document.querySelector(".navbar__toggle-btn");
+
+nav_menu.addEventListener("click", () => {
+  document.querySelector(".navbar__menu").classList.toggle("open");
+});
